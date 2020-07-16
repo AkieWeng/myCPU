95182a0ac8c5d85935cdd6f1698ef384a05997fd
Akie <2994047174@qq.com>
2020-07-16 22:19:39 +0800

    aluctrl&aluctrltest

--- aluctrl.v
+++ aluctrl.v
@@ -1,7 +1,7 @@
 module aluctrl(aluop, funct, aluctrl);
-    input   [1:0]   aluop;
+    input   [2:0]   aluop;
     input   [5:0]   funct;
-    output  [3:0]   aluctrl;
+    output  [2:0]   aluctrl;
 
     assign aluctrl =
         aluop == 3'b000 ? 3'h2 :
@@ -10,12 +10,12 @@ module aluctrl(aluop, funct, aluctrl);
         aluop == 3'b100 ? 3'h4 :
         aluop == 3'b010 ?
         (
-            funct == 6'xx0000 ? 3'h2 :
-            funct == 6'xx0010 ? 3'h6 :
-            funct == 6'xx0100 ? 3'h0 :
-            funct == 6'xx0101 ? 3'h1 :
-            funct == 6'xx1010 ? 3'h7 :
-            funct == 6'xx0110 ? 3'h4 :
+            funct == 6'b100000 ? 3'h2 :
+            funct == 6'b100010 ? 3'h6 :
+            funct == 6'b100100 ? 3'h0 :
+            funct == 6'b100101 ? 3'h1 :
+            funct == 6'b101010 ? 3'h7 :
+            funct == 6'b100110 ? 3'h4 :
                                 3'hx
         ) : 3'hx;
 endmodule
--- aluctrltest.v
+++ aluctrltest.v
@@ -1,3 +1,66 @@
+`timescale 1ns/1ps
+
 module aluctrltest();
-    
+    reg     [2:0]   aluop;
+    reg     [5:0]   funct;
+    wire    [2:0]   aluctrl;
+
+    aluctrl U1
+    (
+        .aluop(aluop),
+        .funct(funct),
+        .aluctrl(aluctrl)
+    );
+
+    initial
+    begin
+        aluop = 3'b010;
+        funct = 6'b100000;
+        // 2
+
+        #20
+        aluop = 3'b010;
+        funct = 6'b100010;
+        // 6
+
+        #20
+        aluop = 3'b010;
+        funct = 6'b100100;
+        // 0
+        
+        #20
+        aluop = 3'b010;
+        funct = 6'b100101;
+        // 1
+
+        #20
+        aluop = 3'b010;
+        funct = 6'b101010;
+        // 7
+
+        #20
+        aluop = 3'b010;
+        funct = 6'b100110;
+        // 4
+
+        #20
+        aluop = 3'b000;
+        funct = 6'b100110;
+        // 2
+
+        #20
+        aluop = 3'b001;
+        funct = 6'b100110;
+        // 6
+
+        #20
+        aluop = 3'b011;
+        funct = 6'b100110;
+        // 1
+
+        #20
+        aluop = 3'b100;
+        funct = 6'b100110;
+        // 4
+    end
 endmodule
